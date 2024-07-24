2e0ffb14e44b6128d24c6940bb6aae30f56245b9
Amit <3540115+amitdeshmukh@users.noreply.github.com>
2024-07-24 10:56:04 +0530

    feat: adds support for additional AxAIs

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,10 @@ const ANTHROPIC_API_KEY: string | undefined = process.env.ANTHROPIC_API_KEY;
 const OPENAI_API_KEY: string | undefined = process.env.OPENAI_API_KEY;
 const COHERE_API_KEY: string | undefined = process.env.COHERE_API_KEY;
 const GEMINI_API_KEY: string | undefined = process.env.GEMINI_API_KEY;
+const GROQ_API_KEY: string | undefined = process.env.GROQ_API_KEY;
+const TOGETHER_API_KEY: string | undefined = process.env.TOGETHER_API_KEY;
+const MISTRAL_API_KEY: string | undefined = process.env.MISTRAL_API_KEY;
+const HUGGINGFACE_API_KEY: string | undefined = process.env.HUGGINGFACE_API_KEY;
 
 interface ProviderApiKeys {
   [key: string]: string | undefined;
@@ -16,6 +20,10 @@ const PROVIDER_API_KEYS: ProviderApiKeys = {
   GEMINI_API_KEY,
   OPENAI_API_KEY,
   ANTHROPIC_API_KEY,
+  GROQ_API_KEY,
+  TOGETHER_API_KEY,
+  MISTRAL_API_KEY,
+  HUGGINGFACE_API_KEY
 };
 
 export {
